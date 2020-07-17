b853f5a47330113ce5730e273d56335fb8ed29c8
Soochin Kang <soochink@uci.edu>
2020-07-16 19:08:06 -0700

    Campground Authorization added

--- YelpCamp/v10/routes/campgrounds.js
+++ YelpCamp/v10/routes/campgrounds.js
@@ -60,6 +60,37 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// EDIT campground route
+router.get("/:id/edit", checkCampgroundOwnership, (req, res) => {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        res.render("campgrounds/edit", { campground: foundCampground });
+    });
+});
+
+// UPDATE campground route
+router.put("/:id", checkCampgroundOwnership, (req, res) => {
+    // find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+    // redirect somewhere (show page)
+});
+
+// DESTROY campground route
+router.delete("/:id", checkCampgroundOwnership, (req, res) => {
+    Campground.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
@@ -67,6 +98,25 @@ function isLoggedIn(req, res, next) {
     }
 
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, (err, foundCampground) => {
+            if (err) {
+                res.redirect("back");
+            } else {
+                // does user own campground?
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
